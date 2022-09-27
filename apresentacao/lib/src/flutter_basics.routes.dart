72939e8d6d70a8f26cb0eda25cc97898908e3fc4
Kaique <munhoz13km@gmail.com>
2022-09-27 06:58:23 -0300

    feat: Create FlutterBasicsRoutes

---
+++ apresentacao/lib/src/flutter_basics.routes.dart
@@ -0,0 +1,14 @@
+import 'package:apresentacao/src/presentation/views/login/login.dart';
+import 'package:flutter/material.dart';
+
+abstract class FlutterBasicsRoutes {
+  static const String initialRoute = FlutterBasicsRoutesNames.login;
+
+  static Map<String, WidgetBuilder> routes = {
+    FlutterBasicsRoutesNames.login: (_) => const Login(),
+  };
+}
+
+abstract class FlutterBasicsRoutesNames {
+  static const login = "login";
+}
