8330a7777aae082d63c7e333c3eb7a5f7165079e
Kaique <munhoz13km@gmail.com>
2022-09-27 07:51:40 -0300

    feat: Add SingleChildScrollView and a sketch of form

--- apresentacao/lib/src/presentation/pages/login/login.page.dart
+++ apresentacao/lib/src/presentation/pages/login/login.page.dart
@@ -9,19 +9,41 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     FlareController _teddyController = FlareControls();
+    EdgeInsets devicePadding = MediaQuery.of(context).padding;
 
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 200,
-          padding: const EdgeInsets.symmetric(horizontal: 30.0),
-          child: FlareActor(
-            "assets/Teddy.flr",
-            shouldClip: false,
-            alignment: Alignment.bottomCenter,
-            fit: BoxFit.contain,
-            controller: _teddyController,
-          ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(
+          left: 20.0,
+          right: 20.0,
+          top: devicePadding.top + 50.0,
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: FlareActor(
+                "assets/Teddy.flr",
+                shouldClip: false,
+                alignment: Alignment.bottomCenter,
+                fit: BoxFit.contain,
+                controller: _teddyController,
+              ),
+            ),
+            Container(
+              width: 200,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(25.0),
+                ),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(30.0),
+              ),
+            ),
+          ],
         ),
       ),
     );
