a90c69d9e419cbfb8ac85b7b5666a904eb6d9af7
Kaique <munhoz13km@gmail.com>
2022-09-29 08:02:14 -0300

    feat: Add two new parameters to CustomTextFormField

--- apresentacao/lib/src/presentation/pages/login/widgets/custom_text_form_field.widget.dart
+++ apresentacao/lib/src/presentation/pages/login/widgets/custom_text_form_field.widget.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class CustomTextFormField extends StatelessWidget {
   static const Color _textFormFieldColor = Color(0xFF5d8e9b);
   final String labelText;
+  final TextEditingController? controller;
+  final bool obscureText;
 
   const CustomTextFormField({
     Key? key,
     required this.labelText,
+    this.controller,
+    this.obscureText = false,
   }) : super(key: key);
 
   @override
