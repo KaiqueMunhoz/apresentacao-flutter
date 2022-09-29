168f1d20f33e1fea08251d0cd9c550ba0678ecf4
Kaique <munhoz13km@gmail.com>
2022-09-29 07:18:54 -0300

    feat: Create TeddyBearGesturesConstants

---
+++ apresentacao/lib/src/presentation/constants/teddy_bear_gestures.constants.dart
@@ -0,0 +1,7 @@
+class TeddyBearGesturesConstants {
+  static const String fail = 'fail';
+  static const String handsDown = 'hands_down';
+  static const String handsUp = 'hands_up';
+  static const String idle = 'idle';
+  static const String success = 'success';
+}
