5d3b5ae414eb2a14f281625b6d80c8bcde255c01
Kaique Munhoz <munhoz13km@gmail.com>
2022-10-04 08:58:12 -0300

    feat: Add backgroundColor and padding to aboutMe page

--- apresentacao/lib/src/features/about_me/presentation/pages/about_me.dart
+++ apresentacao/lib/src/features/about_me/presentation/pages/about_me.dart
@@ -1,12 +1,28 @@
+import 'package:apresentacao/src/features/utils/get_padding_top.dart';
 import 'package:flutter/material.dart';
 
 class AboutMe extends StatelessWidget {
-  const AboutMe({ Key? key }) : super(key: key);
+  const AboutMe({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    const Color _backgroundColor = Color(0xFF5d8e9b);
+    const double _paddingHorizontal = 20.0;
+
+    final double _paddingTop = getPaddingTop(context);
+
     return Scaffold(
-      body: Container(),
+      backgroundColor: _backgroundColor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: _paddingHorizontal),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: _paddingTop),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
