dca5b6f688cbddd000b2b13d4fed2099e02d773c
KaiqueMunhoz <munhoz13km@gmail.com>
2022-10-06 20:49:17 -0300

    feat: Add a new hardcoded list to TasksList

--- todoey_presentation_completed/lib/widgets/tasks_list.dart
+++ todoey_presentation_completed/lib/widgets/tasks_list.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/models/task.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
-import 'package:provider/provider.dart';
-import 'package:todoey_flutter/models/task_data.dart';
 
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<TaskData>(
-      builder: (context, taskData, child) {
-        return ListView.builder(
-          itemBuilder: (context, index) {
-            final task = taskData.tasks[index];
-            return TaskTile(
-              taskTitle: task.name,
-            );
-          },
-          itemCount: taskData.taskCount,
+    final List<Task> _tasks = <Task>[
+      Task(name: 'Comprar leite'),
+      Task(name: 'Comprar banana'),
+      Task(name: 'Comprar um copo'),
+    ];
+
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final task = _tasks[index];
+        return TaskTile(
+          taskTitle: task.name,
         );
       },
+      itemCount: _tasks.length,
     );
   }
 }
