7ea74809857a37b512f6b8520bc3f283ba534b16
Kaique Munhoz <munhoz13km@gmail.com>
2023-09-26 12:56:20 -0300

    feat: Add a backgroundColor and two FloatingActionButtons to about me page

--- apresentacao/lib/src/features/about_me/presentation/pages/about_me.dart
+++ apresentacao/lib/src/features/about_me/presentation/pages/about_me.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 class AboutMe extends StatelessWidget {
-  const AboutMe({ Key? key }) : super(key: key);
+  static const Color _backgroundColor = Color(0xFF5d8e9b);
+
+  const AboutMe({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: _backgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FloatingActionButton(
+                  key: const Key('arrow_back'),
+                  onPressed: () {},
+                  child: const Icon(Icons.arrow_back),
+                  heroTag: null,
+                ),
+                FloatingActionButton(
+                  key: const Key('arrow_forward'),
+                  onPressed: () {},
+                  child: const Icon(Icons.arrow_forward),
+                  heroTag: null,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
