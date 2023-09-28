4489fdfd66963baac2b7dcc636989ef58b56f58d
Kaique Munhoz <munhoz13km@gmail.com>
2023-09-28 10:49:03 -0300

    feat: Create TasksListWidget

---
+++ todoey_presentation/lib/widgets/tasks_list_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:todoey_presentation/widgets/task_widget.dart';
+
+class TasksListWidget extends StatelessWidget {
+  const TasksListWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: ListView(
+          children: const [
+            TaskWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
