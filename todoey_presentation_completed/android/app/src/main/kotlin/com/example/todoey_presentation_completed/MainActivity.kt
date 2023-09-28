84767ca7ce565f9d80feb9e43ce67d8879bb49b6
Kaique Munhoz <munhoz13km@gmail.com>
2023-09-28 08:10:58 -0300

    feat: Remove todoey_presentation_completed

--- todoey_presentation_completed/android/app/src/main/kotlin/com/example/todoey_presentation_completed/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.todoey_presentation_completed
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- todoey_presentation_completed/lib/models/task.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Task {
-  final String name;
-
-  Task({@required this.name});
-}
