c3df853f9d2c39e53b1d2ed062d9dea164e8cbc0
Kaique Munhoz <munhoz13km@gmail.com>
2023-09-28 09:44:15 -0300

    feat: Wrap the first Column inside another Column, and move expanded to first Column

--- todoey_presentation/lib/screens/tasks_screen.dart
+++ todoey_presentation/lib/screens/tasks_screen.dart
@@ -7,54 +7,58 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue,
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 60.0,
-          right: 30.0,
-          bottom: 30.0,
-          left: 30.0,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 30,
-              child: Icon(
-                Icons.list,
-                size: 30.0,
-                color: Colors.lightBlue,
-              ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 60.0,
+              right: 30.0,
+              bottom: 30.0,
+              left: 30.0,
             ),
-            const SizedBox(height: 10.0),
-            const Text(
-              'Todoey',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 60.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const Text(
-              '12 tasks',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.0),
-                    topRight: Radius.circular(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlue,
                   ),
                 ),
+                const SizedBox(height: 10.0),
+                const Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 60.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  '12 tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
