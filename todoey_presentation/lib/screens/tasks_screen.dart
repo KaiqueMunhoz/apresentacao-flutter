3aa69d9060c1179a3a30f5c8c6b78f9f8bb671a5
Kaique Munhoz <munhoz13km@gmail.com>
2023-09-28 08:57:49 -0300

    feat: Wrap Column with Padding

--- todoey_presentation/lib/screens/tasks_screen.dart
+++ todoey_presentation/lib/screens/tasks_screen.dart
@@ -7,11 +7,19 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue,
-      body: Column(
-        children: const [
-          Icon(Icons.list),
-          Text('Todoey'),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 60.0,
+          right: 30.0,
+          bottom: 30.0,
+          left: 30.0,
+        ),
+        child: Column(
+          children: const [
+            Icon(Icons.list),
+            Text('Todoey'),
+          ],
+        ),
       ),
     );
   }
